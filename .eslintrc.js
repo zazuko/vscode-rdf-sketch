ff2fc54ae488f5bfee42de9f6d2136d4b78ec26d
Martin Maillard <git@martinmaillard.com>
2021-08-09 09:45:10 +0200

    Setup eslint typescript+standard config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,12 @@
-/**@type {import('eslint').Linter.Config} */
+/** @type {import('eslint').Linter.Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  plugins: [],
+  extends: 'standard-with-typescript',
   rules: {
-    'semi': [2, "always"],
+    semi: [2, 'always'],
     '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
